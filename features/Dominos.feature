72a285c89f9e0b4606dd1c360c9bfea2d870b7ab
Seethasammpath <ssammpath.gannet.com>
2014-12-16 17:08:16 -0500

    2nd scenario

--- features/Dominos.feature
+++ features/Dominos.feature
@@ -2,11 +2,23 @@ Feature: Order Dominos Pizza Online
 We want to step through a simple scenario for training
 
 Background:
-When I visit the Dominos home page
+When I visit the Dominos home page 
 
 @orderonlinenav
 Scenario: Check the Order Online page
 Given I am on the Dominos home page
 When I click on 'Order Online' link
-Then I should be on 'Order Online' home page
+Then I should be on'Order Online' page
+
+@Fillingaddress
+Scenario: Filling out the Address Information
+Given I am on'Order Online'page
+When I fill out Address Information form
+And I click on Search Location button
+Then I should be on locationResults page
+
+
+
+
+
 
--- features/step_definitions/Dominos_steps.rb
+++ features/step_definitions/Dominos_steps.rb
@@ -2,18 +2,38 @@
 When(/^I visit the Dominos home page$/) do
 visit("https://order.dominos.com")
 end
-#test1_OrderOnlineAddress
+
 Given(/^I am on the Dominos home page$/) do
-#expect(page).to have_text("MENU")
-#expect(page).to have_css("#pageContent")
 expect(page).to have_selector(:id, 'homePage')
 end
+
 When(/^I click on 'Order Online' link$/) do
 find(".qa-Cl_order").click
 sleep 20
 end
-Then(/^I should be on 'Order Online' home page$/) do
-#expect(page).to have_text "ADDRESS INFORMATION"
-#expect(page).to have_css("#pageContent")
+
+Then(/^I should be on'Order Online' page$/) do
 expect(page).to have_selector(:id, 'locationsSearchPage')
 end
+
+Given(/^I am on'Order Online'page$/) do
+find(".qa-Cl_order").click
+sleep 20
+end
+
+When(/^I fill out Address Information form$/) do
+  find(".Carryout").click
+ select('House', :from => 'Address Type')
+ fill_in('City', :with => 'Herndon')
+ select('VA', :from => 'State')
+ fill_in('Zip Code', :with => '20171')
+end
+
+When(/^I click on Search Location button$/) do
+   find(".btn--large").click
+end
+
+Then(/^I should be on locationResults page$/) do
+     expect(page).to have_css("#locationsResultsPage")
+ end
+
