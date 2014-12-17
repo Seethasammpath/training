7d6c9627e53d981aef9db6985a3563f50545daaf
Seethasammpath <ssammpath.gannet.com>
2014-12-17 16:34:41 -0500

    added 3&4scenario

--- features/Dominos.feature
+++ features/Dominos.feature
@@ -12,10 +12,36 @@ Then I should be on'Order Online' page
 
 @Fillingaddress
 Scenario: Filling out the Address Information
-Given I am on'Order Online'page
+When I click on 'Order Online' link
 When I fill out Address Information form
 And I click on Search Location button
 Then I should be on locationResults page
+
+@Selectdeliverystore 
+Scenario:Selecting delivery store
+When I click on 'Order Online' link
+When I fill out Address Information form
+And I click on Search Location button
+And I click on FutureCarryoutorder button 
+Then i should be on the Entrees page 
+
+
+@ordermultiplepizzas
+Scenario:Ordering multiple pizza
+When I click on 'Order Online' link
+When I fill out Address Information form
+And I click on Search Location button
+And I click on FutureCarryoutorder button 
+And I click on Specialty Pizza
+And I click on order link
+And I click on Addtoorder link 
+Then i should be on the Entrees page 
+
+
+
+
+
+
 
 
 
--- features/step_definitions/Dominos_steps.rb
+++ features/step_definitions/Dominos_steps.rb
@@ -1,4 +1,4 @@
-#Background step:
+#scenario:1
 When(/^I visit the Dominos home page$/) do
 visit("https://order.dominos.com")
 end
@@ -15,10 +15,10 @@ end
 Then(/^I should be on'Order Online' page$/) do
 expect(page).to have_selector(:id, 'locationsSearchPage')
 end
-
+#scenario:2
 Given(/^I am on'Order Online'page$/) do
 find(".qa-Cl_order").click
-sleep 20
+sleep 5
 end
 
 When(/^I fill out Address Information form$/) do
@@ -36,4 +36,38 @@ end
 Then(/^I should be on locationResults page$/) do
      expect(page).to have_css("#locationsResultsPage")
  end
+
+ #scenario:3
+
+#For selecting store in 3rd position 
+
+#When(/^I select store in '(\d+)' position$/) do |arg1|
+ #find(:xpath, "//div[@id='locationsResultsPage']/div[2]/div[2]/div[3]/div[2]/a").click
+#end
+
+#By default this will select store in first position 
+And(/^I click on FutureCarryoutorder button$/) do
+	first('.js-orderCarryoutNow').click
+ end
+
+Then(/^i should be on the Entrees page$/) do
+	expect(page).to have_css('#entreesPage')
+ end
+
+#Scenario:4
+When(/^I click on Specialty Pizza$/) do
+ find('.navigation-Pizza').click
+end
+
+When(/^I click on order link$/) do
+first("div[class='button']", :text =>"Order").click
+end
+
+When(/^I click on Addtoorder link$/) do
+  find(".btn js-isNew").click
+end
+
+
+
+
 
